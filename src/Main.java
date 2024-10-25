f3d9754f65a46ee774abf8a8c323489560d26b54
Kateryna Nuzhna <katranchik21@gmail.com>
2024-10-25 19:03:27 +0200

    finaly code 2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -12,14 +13,19 @@ Student и отсортировать его по среднеарифметич
 public class Main {
     public static void main(String[] args) {
 
-        List<Student> students = new ArrayList<>();
+       // List<Student> students = new ArrayList<>();
 
         // Додавання студентів до списку
-        students.add(new Student("Аліса", List.of(83, 78, 70)));
-        students.add(new Student("Іван", List.of(67, 86, 81)));
-        students.add(new Student("Богдан", List.of(92, 90, 91)));
+        Student[] students = {
+                new Student("Аліса", List.of(83, 78, 70)),
+                new Student("Іван", List.of(67, 86, 81)),
+                new Student("Богдан", List.of(92, 90, 91))
+      };
+        print(students);
+        Arrays.sort(students);
+          print(students);
 
-
+/*
         // Сортування студентів за середньоарифметичним значенням оцінок
         for (int i = 0; i < students.size() - 1; i++) {
             for (int j = 0; j < students.size() - 1 - i; j++) {
@@ -32,6 +38,11 @@ public class Main {
         }
         // Виведення відсортованого списку студентів
         System.out.println();
-        for (Student student : students) System.out.println(student);
+        for (Student student : students) System.out.println(student);*/
+    }
+    public static void print(Student[] students){
+        for (Student student:students){
+            System.out.println(student);
+        }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,22 +3,24 @@ import java.util.List;
 /*
 Создайте класс Student (String name, List<Integer> rates) имя, лист оценок.
  */
-public class Student {
+public class Student implements Comparable<Student>{
     private final String name;
-    List<Integer> rates;
+    private List<Integer> rates;
+    private double avg;
 
     public Student(String name, List<Integer> rates) {
         this.name = name;
         this.rates = rates;
+        this.avg = avgRate();
     }
 
-    public String getName() {
+   /* public String getName() {
         return name;
     }
 
     public List<Integer> getRates() {
         return rates;
-    }
+    }*/
 
 
     // Метод для обчислення середньоарифметичного значення оцінок
@@ -39,7 +41,30 @@ public class Student {
     public String toString() {
         return "Студент " +
                 "по імені: " + name  +
-                ", оцінки: " + rates +
+                ", оцінки: " + rates + " (" + avg + ")"+
                 System.lineSeparator();
+    }
+
+    public double avgRate(){
+        if (rates==null || rates.size()==0){//priverka na null
+            return 0;
+        }//serednearufmetu4ne
+        double sum =0.0;
+        for (Integer i : rates){
+            sum += i;
+        }
+        return sum/rates.size();
+    }
+    //metod dlja sortirovki
+    @Override
+    public int compareTo(Student otherStudent) {
+        return Double.compare(avg, otherStudent.avg);//sravnit' 2 dabla
+      /*  if (avg >otherStudent.avg){
+            return 1;
+        }else if (avg<otherStudent.avg){
+            return -1;
+        }else {
+            return 0;
+        }//dobavili avg dlja ekonomii vremeni podsweta*/
     }
 }
